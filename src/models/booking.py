ccb7e39020387e3fa4566638a9b98c9a59657045
GalinaLody <Lodyginagl@gmail.com>
2026-08-13 21:29:13 +0000

    fix booking model

--- src/models/booking.py
+++ src/models/booking.py
@@ -1,12 +1,14 @@
 import uuid
 from datetime import date
 from enum import StrEnum
-from typing import Optional
+from typing import List, Optional
 
 from sqlalchemy import UUID, Date, Enum, ForeignKey, Integer, String
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
-from src.core.base_model import Base
+from core.base_model import Base
+from src.models.slots import Slot
+from src.models.table import Table
 
 
 class StatusBooking(StrEnum):
@@ -27,22 +29,25 @@ class BookingTablesSlots(Base):
 
     booking_id: Mapped[uuid.UUID] = mapped_column(
         UUID,
-        ForeignKey('booking.id', name='fk_booking_id_booking_tables_slots'),
+        ForeignKey('bookings.id', name='fk_booking_id_booking_tables_slots'),
     )
+    booking: Mapped['Booking'] = relationship('Booking', back_populates='table_slot', lazy='selectin')
     table_id: Mapped[uuid.UUID] = mapped_column(
         UUID,
         ForeignKey('tables.id', name='fk_tables_id_booking_tables_slots'),
     )
+    table: Mapped['Table'] = relationship('Table', back_populates='booking_tables_slots', lazy='selectin')
     slot_id: Mapped[uuid.UUID] = mapped_column(
         UUID,
         ForeignKey('slots.id', name='fk_slots_id_booking_tables_slots'),
     )
+    slot: Mapped['Slot'] = relationship('Slot', back_populates='booking_tables_slots', lazy='selectin')
 
 
 class Booking(Base):
     """Модель бронирования заказов."""
 
-    user_id: Mapped[uuid.UUID] = mapped_column(UUID, ForeignKey('user.id', name='fk_booking_user_id_user'))
+    user_id: Mapped[uuid.UUID] = mapped_column(UUID, ForeignKey('users.id', name='fk_booking_user_id_user'))
     cafe_id: Mapped[uuid.UUID] = mapped_column(UUID, ForeignKey('cafes.id', name='fk_booking_cafe_id_cafes'))
     booking_date: Mapped[date] = mapped_column(Date)
     guest_number: Mapped[int] = mapped_column(Integer)
@@ -51,4 +56,8 @@ class Booking(Base):
         Enum(StatusBooking, name='status_booking_enum'),
         default=StatusBooking.BOOKING,
         server_default=StatusBooking.BOOKING.value,
+    )
+    table_slot: Mapped[List['BookingTablesSlots']] = relationship(
+        'BookingTablesSlots',
+        back_populates='booking',
     )
