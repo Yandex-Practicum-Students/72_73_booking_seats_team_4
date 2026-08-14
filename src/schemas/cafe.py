65ffa8e6b31dcf8622fbaf650522068e279aa433
KvazyModa00 <117615404+KvazyModa00@users.noreply.github.com>
2026-08-14 17:09:21 +0300

    add models_cafe, schema_cafe, base_schema, edit base_model, (#33)
    * add models_cafe, schema_cafe, base_schema, edit base_model,
    
    * fix BaseSchemaDB
    
    * Delete infra/.env — копия.example:Zone.Identifier
    
    * edit base_model, cafe_schema, cafe_models
    
    * delete managers_schema from cafe_schemas
    
    * edit is_active schema in base_schemas, cafe_Schemas
    
    * ad fk name to came_model

---
+++ src/schemas/cafe.py
@@ -0,0 +1,43 @@
+import uuid
+from typing import List, Optional
+
+from pydantic import BaseModel
+
+from models.user import User  # noqa
+
+from src.schemas.base import BaseSchemaDB, DescriptionSchema, FullBaseSchemaDB, IsActiveSchema
+
+
+class BaseCafe(BaseModel):
+    """Базовая схема объекта кафе."""
+
+    name: str
+    address: str
+    phone: str
+    photo_id: Optional[uuid.UUID] = None
+
+
+class CafeCreate(DescriptionSchema, BaseCafe):
+    """Схема создания объекта."""
+
+    managers_id: List[uuid.UUID]
+
+
+class CafeInfo(FullBaseSchemaDB, BaseCafe, DescriptionSchema):
+    """Схема полной инфо об объекте."""
+
+    managers: List[User]  # noqa
+
+
+class CafeShortInfo(BaseSchemaDB, BaseCafe, DescriptionSchema):
+    """Схема для получения короткой инфо о объекте."""
+
+
+class CafeUpdate(DescriptionSchema, IsActiveSchema):
+    """Схема обновления."""
+
+    name: Optional[str] = None
+    address: Optional[str] = None
+    phone: Optional[str] = None
+    photo_id: Optional[uuid.UUID] = None
+    managers_id: Optional[List[uuid.UUID]] = None
