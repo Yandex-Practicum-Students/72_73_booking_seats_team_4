bf61e2c73d72ad9fcc2294ef59a87b87f3e681fa
Tatarchuk Veronika <veronik4578@gmail.com>
2026-08-15 15:29:29 +0300

    Refactored Table schemas to inherit from base schemas (base.py)

--- src/core/alembic_models.py
+++ src/core/alembic_models.py
@@ -1,5 +1,7 @@
 from models.action import Action, CafeAction
 from models.dish import CafeDish, Dish
+from models.stots import Stots
+from models.table import Table
 
 from core.base_model import Base
 
@@ -9,4 +11,6 @@ __all__ = [
     CafeAction,
     CafeDish,
     Dish,
+    Stots,
+    Table,
 ]
--- src/models/table.py
+++ src/models/table.py
@@ -1,43 +1,35 @@
 import uuid
-from typing import TYPE_CHECKING
 
-from sqlalchemy import UUID, ForeignKey, Integer, Text
+from sqlalchemy import UUID, ForeignKey, Integer
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
-from core.base_model import Base
-
+from core.base_model import Base, DescriptionMixin
 from src.models.booking import BookingTablesSlots
 from src.models.cafe import Cafe
 
 
-class Table(Base):
+class Table(Base, DescriptionMixin):
     """Модель стола в кафе."""
 
     cafe_id: Mapped[uuid.UUID] = mapped_column(
         UUID,
-        ForeignKey('cafes.id', ondelete='NO ACTION'),
+        ForeignKey("cafes.id", ondelete="NO ACTION"),
         nullable=False,
     )
     seat_number: Mapped[int] = mapped_column(
         Integer,
         nullable=False,
     )
-    description: Mapped[str | None] = mapped_column(
-        Text,
-        nullable=True,
-    )
-
-
     cafe: Mapped["Cafe"] = relationship(
         "Cafe",
         back_populates="tables",
         lazy="selectin",
     )
     bookings: Mapped[list["BookingTablesSlots"]] = relationship(
-        "Booking",
+        "BookingTablesSlots",
         back_populates="table",
         lazy="selectin",
     )
 
     def __repr__(self) -> str:
-        return f'Стол {self.id} (кафе={self.cafe_id}, мест={self.seat_number})'
+        return f"Стол {self.id} (кафе={self.cafe_id}, мест={self.seat_number})"
