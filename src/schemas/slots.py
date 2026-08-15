bd418dd3445db3d6fb8666ff4dc588fe0f190bef
Tatarchuk Veronika <veronik4578@gmail.com>
2026-08-15 17:42:06 +0300

    Refactored Slots schemas to inherit from base schemas (base.py) slots_validator is placed into validator.

--- src/schemas/slots.py
+++ src/schemas/slots.py
@@ -2,59 +2,42 @@ from datetime import time
 from typing import Annotated, Optional
 from uuid import UUID
 
-from pydantic import BeforeValidator, Field
+from pydantic import BaseModel, BeforeValidator, Field
 
-from core.base_model import Base
-from src.schemas.slots_validators import TimeValidatorMixin, normalize_time
+from src.schemas.base import BaseSchemaDB, DescriptionSchema, FullBaseSchemaDB
+from src.schemas.validators import TimeValidatorMixin, normalize_time
 
 StartTime = Annotated[time, BeforeValidator(normalize_time)]
 EndTime = Annotated[time, BeforeValidator(normalize_time)]
 
 
-class SlotBase(TimeValidatorMixin, Base):
+class SlotBase(TimeValidatorMixin, BaseModel , DescriptionSchema):
     """Базовая схема слота."""
 
-    start_time: StartTime = Field(description="Время начала")
-    end_time: EndTime = Field(description="Время окончания")
-    description: Optional[str] = Field(None, description="Описание слота")
+    start_time: StartTime = Field(description='Время начала')
+    end_time: EndTime = Field(description='Время окончания')
 
 
 class SlotCreate(SlotBase):
     """Схема для создания слота."""
 
-    cafe_id: UUID = Field(description="ID кафе")
+    cafe_id: UUID = Field(description='ID кафе')
 
 
 class SlotUpdate(SlotBase):
     """Схема для обновления слота."""
 
-    start_time: Optional[StartTime] = Field(None, description="Время начала")
-    end_time: Optional[EndTime] = Field(None, description="Время окончания")
-    is_active: Optional[bool] = Field(None, description="Активность слота")
+    start_time: Optional[StartTime] = Field(None, description='Время начала')
+    end_time: Optional[EndTime] = Field(None, description='Время окончания')
+    is_active: Optional[bool] = Field(None, description='Активность слота')
 
 
-class SlotInfo(Base):
-    """Полная информация о слоте."""
-
-    cafe_id: UUID
-    cafe_name: Optional[str] = None
-    start_time: time
-    end_time: time
-    description: Optional[str] = None
-
-    class Config:
-        """Конфигурация Pydantic модели."""
-
-        from_attributes = True
-
-
-class SlotShort(Base):
+class SlotShort(BaseSchemaDB, SlotBase):
     """Краткая информация о слоте."""
 
-    start_time: time
-    end_time: time
 
-    class Config:
-        """Конфигурация Pydantic модели."""
+class SlotInfo(FullBaseSchemaDB, SlotBase):
+    """Полная информация о слоте."""
 
-        from_attributes = True
+    cafe_id: UUID = Field(description='ID кафе')
+    cafe_name: Optional[str] = Field(None, description='Название кафе')
