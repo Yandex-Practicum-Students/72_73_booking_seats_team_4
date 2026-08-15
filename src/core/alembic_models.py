9744a783b599040b41134f2cf2ffe7ec1ca828f5
Tatarchuk Veronika <veronik4578@gmail.com>
2026-08-15 16:24:26 +0300

    sort list "__all__"

--- src/core/alembic_models.py
+++ src/core/alembic_models.py
@@ -1,13 +1,14 @@
+from models.stots import Stots
+
 from models.action import Action, CafeAction
 from models.dish import CafeDish, Dish
-from models.stots import Stots
 from models.table import Table
 
 from core.base_model import Base
 
 __all__ = [
-    Base,
     Action,
+    Base,
     CafeAction,
     CafeDish,
     Dish,
