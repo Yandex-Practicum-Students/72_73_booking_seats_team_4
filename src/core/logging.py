deded176d45a0ed09d0cc5daae465e25f572e070
Roman Papenov <roman82sp@yandex.ru>
2026-08-16 21:03:18 +0000

    Add logging into stdin & logs/app.log

--- src/core/logging.py
+++ src/core/logging.py
@@ -2,31 +2,28 @@ import sys
 
 from loguru import logger
 
+LOG_FORMAT = (
+    '{time:YYYY-MM-DD HH:mm:ss.SSS} | '
+    '{level: <8} | '
+    # '{extra[user_id]} - '
+    # '{extra[username]} | '
+    '{name} | '
+    '{message}'
+)
+
 
 def configure_loguru() -> None:
     """Функция настройки параметров логгирования."""
     logger.remove()
 
-    logger.add(
-        sys.stdout,
-        level='INFO',
-        format='{time:YYYY-MM-DD HH:mm:ss.SSS} | '
-        '{level} | '
-        # '{extra[user_id]} - '
-        # '{extra[username]} | '
-        '{name} | '
-        '{message}',
-        enqueue=True,
-    )
+    logger.add(sys.stdout, level='INFO', format=LOG_FORMAT, enqueue=True)
 
     logger.add(
         'logs/app.log',
+        rotation='10 MB',
+        retention=5,
         level='INFO',
-        format='{time:YYYY-MM-DD HH:mm:ss.SSS} | '
-        '{level} | '
-        # '{extra[user_id]} - '
-        # '{extra[username]} | '
-        '{name} | '
-        '{message}',
+        format=LOG_FORMAT,
         enqueue=True,
+        encoding='utf-8',
     )
