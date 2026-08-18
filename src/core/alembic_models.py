75415e5af78a84224265b6d66818d279c12316f8
Alexey Kosykhin <aleksksn@yahoo.co.uk>
2026-08-18 04:27:11 +0100

    remove stray text from alembic_models.py

--- src/core/alembic_models.py
+++ src/core/alembic_models.py
@@ -1,10 +1,10 @@
-python
 # from models.slots import Slot
 from models.action import Action, CafeAction
 from models.cafe import Cafe
 from models.dish import CafeDish, Dish
 from models.media import Media
 from models.table import Table
+
 from core.base_model import Base
 
 __all__ = [
